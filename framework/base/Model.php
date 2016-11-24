a6cc52f0b989757aaaaedd40de739009858db5e9
hoanvv <hoanvv.it@gmail.com>
2016-11-24 16:16:15 +0700

    review and edit address model

--- framework/base/Model.php
+++ framework/base/Model.php
@@ -4,7 +4,7 @@ include_once __DIR__ . '/config.php';
 class Model{
 	public function connect(){
 	    try {
-	        $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASSWORD);
+	        $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USER,DB_PASSWORD);
 	        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	    } catch (Exception $ex) {
 	         echo 'Ket noi that bai: ' . $ex->getMessage();
